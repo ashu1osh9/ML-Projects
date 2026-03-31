564647ab652069cc01a2de6cc3bb5dbb729d770b
Ashutosh Kumar <ashutosh.kr1818@.com>
2026-03-31 12:04:06 +0530

    logger and exception

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def get_requirements(file_path:str)->List[str]:
     return requirements
 
 setup(
-name='ML-Project',
+name='ML-Projects',
 version='0.0.1',
 author='Ashutosh',
 author_email='ashutosh.kr1818@gmail.com',
